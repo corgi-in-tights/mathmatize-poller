d981702c60f87269f0fd9f9b1cadc3abf5e03413
corgi-in-tights <59304120+corgi-in-tights@users.noreply.github.com>
2024-09-20 12:36:23 -0400

    updated basic example

--- examples/basic.py
+++ examples/basic.py
@@ -1,11 +1,12 @@
-import os, time, asyncio
+import os, time, asyncio, requests
 from mathmatize_poller import MathMatizePoller
 
-poll_uuid = input('Please enter the UUID in the Poll URL:')
+
+
 
 def on_poll_update(monitor):
-    print ('There was a poll update!')
-    print (time.time() + monitor.duration - monitor.start_time + ' seconds remaining.')
+    print (f'There was a poll update on {monitor.url}!')
+    print(f'{time.time() + monitor.duration - monitor.start_time} seconds remaining...')
 
 """
 Async is required since the poll runs in the background, easy to port to sync (extend PollMonitor) but
@@ -15,30 +16,26 @@ Selenium webdrivers are not thread-safe therefore the driver runs on the main/re
 
 Work can be done in the background but it must be `await`ed to gain the benefits of async.
 """
-async def start_poller(loop, poll_uuid):
+async def start_poller(loop, url):
     poller = MathMatizePoller(
         None, # silently install new chromedriver, best practice is to provide a path to a local chromedriver if avaliable
         os.getenv('MATHMATIZE_EMAIL'),
         os.getenv('MATHMATIZE_PASSWORD')
     )
 
-    monitor = poller.get_or_create_monitor(poll_uuid, on_poll_update, 30, 5, k=1.5)
+    monitor = poller.get_or_create_monitor(url, on_poll_update, 60*60, 12, k=4)
     monitor.start()
-
-    # simulate work
-    await asyncio.sleep(50)
-
-    monitor.stop()
-    poller.shutdown()
-    loop.stop()
 
 
 
 if __name__ == '__main__':
+    url = input('Please enter the poll url:')
+    print ('Selected', url)
+
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     try:
-        loop.create_task(start_poller(loop, poll_uuid))
+        loop.create_task(start_poller(loop, url))
         loop.run_forever()
     except KeyboardInterrupt:
         pass
